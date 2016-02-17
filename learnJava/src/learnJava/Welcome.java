5de87d8e817c8dcabf97bb7cd3cc5a77f44fa18d
Joseph <jomz@home.qqc.com>
2016-02-16 23:14:57 -0800

    test commit

---
+++ learnJava/src/learnJava/Welcome.java
@@ -0,0 +1,10 @@
+package learnJava;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		// Display message Welcome to Java! to the console
+		System.out.println("Welcome to Java!");
+	}
+
+}
